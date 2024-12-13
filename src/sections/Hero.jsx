4f78677fb8b1e9dc98f0b844c9329e8d194c4fe2
aditshah <adit.s@inheritx.com>
2024-12-13 13:12:39 +0530

    added statistics to our hero page - part3

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,22 +1,29 @@
 import Button from "../components/Button";
 import {arrowRight} from "../assets/icons";
-
+import {statistics} from "../constants"
 const Hero = () => {
   return (
-    <section id="home" className="w-full flex xl:flex-row flex-col border-2 justify-center min-h-screen border-red-500 gap-10 max-container p-2">
+    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container p-2">
 
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-      <p className="">Our Summer collections</p>
-      <h1>
-        <span>
+      <p className="text-xl font-montserrat text-coral-red">Our Summer collections</p>
+      <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+        <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
         The New Arrival
         </span>
         <br />
-        <span>Nike</span>
-        Shoes
+        <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
       </h1>
-      <p>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
+      <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm: max-w-sm">Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
       <Button label="Shop now" iconURL = {arrowRight}/>
+      <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+        {statistics.map((stat)=>(
+          <div key={stat.label}>
+            <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+            <p className="leading-7 font-montserrat text-slate-gray">{stat.label}</p>
+          </div>
+        ))}
+      </div>
       </div>
     </section>
   )
