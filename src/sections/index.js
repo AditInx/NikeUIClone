ec69ad5142681eed04cec97510ad2e534723b799
aditshah <adit.s@inheritx.com>
2024-12-13 11:06:47 +0530

    added various sections and imported all of them to index.js file and used them in App.jsx file

---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import SpecialOffers from "./SpecialOffers";
+import Nav from "./Nav";
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffers,
+    Subscribe,
+    Footer,
+    CustomerReviews,
+    Nav
+}
