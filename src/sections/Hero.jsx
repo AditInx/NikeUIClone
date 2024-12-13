400bb04d66fdb990cd6148db428dd14e9d03561a
aditshah <adit.s@inheritx.com>
2024-12-13 12:38:22 +0530

    making of hero section part-1

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,20 @@
 const Hero = () => {
   return (
-    <div>
-      
-    </div>
+    <section id="home" className="w-full flex xl:flex-row flex-col border-2 justify-center min-h-screen border-red-500 gap-10 max-container p-2">
+
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+      <p className="">Our Summer collections</p>
+      <h1>
+        <span>
+        The New Arrival
+        </span>
+        <br />
+        <span>Nike</span>
+        Shoes
+      </h1>
+      <p>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
+      </div>
+    </section>
   )
 }
 
